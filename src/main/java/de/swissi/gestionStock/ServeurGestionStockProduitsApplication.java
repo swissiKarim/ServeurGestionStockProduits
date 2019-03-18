12f49d7027ba54409361dc96f3efa70a06dbecae
Swissi <Mohamed-Karim.Swissi@adesso.de>
2019-03-18 14:39:44 +0100

    first commit

---
+++ src/main/java/de/swissi/gestionStock/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package de.swissi.gestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+
+}
